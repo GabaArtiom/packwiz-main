0a4083d10b80e892e38362573fb8a1201ad24a4f
g.artiom <toioma456@gmail.com>
2026-05-31 14:04:27 +0300

    fix recipe viewer hide script

--- kubejs/client_scripts/hide_allthemodium_gear.js
+++ kubejs/client_scripts/hide_allthemodium_gear.js
@@ -1,4 +1,4 @@
-JEIEvents.hideItems(event => {
+RecipeViewerEvents.removeEntries('item', event => {
   const materials = ['allthemodium', 'vibranium', 'unobtainium']
   const hiddenGear = [
     'helmet',
@@ -13,11 +13,11 @@ JEIEvents.hideItems(event => {
 
   for (const material of materials) {
     for (const gear of hiddenGear) {
-      event.hide(`allthemodium:${material}_${gear}`)
+      event.remove(`allthemodium:${material}_${gear}`)
     }
   }
 
   for (const gear of ['sword', 'axe', 'shovel', 'paxel']) {
-    event.hide(`allthemodium:alloy_${gear}`)
+    event.remove(`allthemodium:alloy_${gear}`)
   }
 })
