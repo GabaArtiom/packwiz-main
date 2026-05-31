8ff1dc70bafebbd13e56557f840d4cf3080483db
g.artiom <toioma456@gmail.com>
2026-05-31 14:17:20 +0300

    hide blocked gear from creative tab

---
+++ kubejs/startup_scripts/hide_allthemodium_creative_tab.js
@@ -0,0 +1,19 @@
+const blockedAllthemodiumGear = []
+
+for (const material of ['allthemodium', 'vibranium', 'unobtainium']) {
+  for (const gear of ['helmet', 'chestplate', 'leggings', 'boots', 'sword', 'pickaxe', 'axe', 'shovel']) {
+    blockedAllthemodiumGear.push(`allthemodium:${material}_${gear}`)
+  }
+}
+
+for (const gear of ['sword', 'axe', 'shovel', 'paxel']) {
+  blockedAllthemodiumGear.push(`allthemodium:alloy_${gear}`)
+}
+
+for (const tab of ['allthemodium:allthemodium', 'allthemodium:allthemodium_tab', 'allthemodium:main']) {
+  StartupEvents.modifyCreativeTab(tab, event => {
+    for (const item of blockedAllthemodiumGear) {
+      event.remove(item)
+    }
+  })
+}
