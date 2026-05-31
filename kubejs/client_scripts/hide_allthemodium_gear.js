16a78b4474ff5d92c9e0af80612344a0255cd739
g.artiom <toioma456@gmail.com>
2026-05-31 13:50:04 +0300

    hide blocked allthemodium gear

---
+++ kubejs/client_scripts/hide_allthemodium_gear.js
@@ -0,0 +1,23 @@
+EMIEvents.hideItems(event => {
+  const materials = ['allthemodium', 'vibranium', 'unobtainium']
+  const hiddenGear = [
+    'helmet',
+    'chestplate',
+    'leggings',
+    'boots',
+    'sword',
+    'pickaxe',
+    'axe',
+    'shovel'
+  ]
+
+  for (const material of materials) {
+    for (const gear of hiddenGear) {
+      event.hide(`allthemodium:${material}_${gear}`)
+    }
+  }
+
+  for (const gear of ['sword', 'axe', 'shovel', 'paxel']) {
+    event.hide(`allthemodium:alloy_${gear}`)
+  }
+})
